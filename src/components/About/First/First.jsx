fbfbff21402cf3c3630af275f4de73ecdc38569f
sadiaprova12 <sadiaprova51717@gmail.com>
2024-04-01 13:14:48 +0600

    added default first page

---
+++ src/components/About/First/First.jsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+const First = () => {
+    return (
+        <div>
+            <h3>This is the default first page.</h3>
+        </div>
+    );
+};
+
+export default First;
