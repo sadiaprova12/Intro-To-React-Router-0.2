9a00c3abe51de9af929eb7d36d49b87f9f8616b0
sadiaprova12 <sadiaprova51717@gmail.com>
2024-04-01 12:57:45 +0600

    about

---
+++ src/components/About/About.jsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h3>This is the real about page.</h3>
+        </div>
+    );
+};
+
+export default About;
