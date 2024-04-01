c204985d2a281de1088b7ca32d2db419040c357d
sadiaprova12 <sadiaprova51717@gmail.com>
2024-04-02 01:42:05 +0600

    data map

--- src/components/About/Friends/Frients.jsx
+++ src/components/About/Friends/Frients.jsx
@@ -2,6 +2,7 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import Friend from '../Friend/Friend';
 
 const Frients = () => {
    
@@ -11,6 +12,14 @@ const Frients = () => {
     return (
         <div>
             <h2>These are my friends: {data.length}</h2>
+            <div>
+                {
+                    data.map(friend => <Friend
+                        key={friend.id}
+                        friend={friend}
+                    ></Friend>)
+                }
+            </div>
         </div>
     );
 };
