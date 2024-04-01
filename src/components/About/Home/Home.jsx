0312caecd975f6f0224ce9da003b95819c432b65
sadiaprova12 <sadiaprova51717@gmail.com>
2024-04-01 12:59:13 +0600

    import outlet and header

---
+++ src/components/About/Home/Home.jsx
@@ -0,0 +1,16 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Header from '../Header/Header';
+
+const Home = () => {
+    return (
+        <div>
+            <Header></Header>
+            <h3>This is home.</h3>
+            <Outlet></Outlet>
+        </div>
+    );
+};
+
+export default Home;
