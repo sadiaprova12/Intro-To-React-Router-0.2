27c9ed3a5ecf3bed0f157fb92694b970070a6d53
sadiaprova12 <sadiaprova51717@gmail.com>
2024-04-02 22:15:25 +0600

    friend use loader data

--- src/components/About/FriendDetail/FriendDetail.jsx
+++ src/components/About/FriendDetail/FriendDetail.jsx
@@ -1,7 +1,10 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 const FriendDetail = () => {
+    const friend = useLoaderData();
+    console.log(friend)
     return (
         <div>
             <h3>Everything about this person is here!</h3>
