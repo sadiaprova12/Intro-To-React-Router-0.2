ce00ac7bb5c532d8b09e269f9f712d9a1512db64
sadiaprova12 <sadiaprova51717@gmail.com>
2024-04-02 19:54:30 +0600

    link to dynamic

--- src/components/About/Friend/Friend.jsx
+++ src/components/About/Friend/Friend.jsx
@@ -1,16 +1,18 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 import './Friend.css';
+import { Link } from 'react-router-dom';
 // eslint-disable-next-line react/prop-types
 const Friend = ({friend}) => {
     console.log(friend);
     // eslint-disable-next-line react/prop-types
-    const {email, name, phone } = friend;
+    const {email, name, id, phone } = friend;
     return (
         <div className='friend'>
             <h3>{name}</h3>
             <p>email: {email}</p>
             <p>phone: {phone}</p>
+            <p><Link to={`/friend/${id}`}>Show me details</Link></p>
         </div>
     );
 };
