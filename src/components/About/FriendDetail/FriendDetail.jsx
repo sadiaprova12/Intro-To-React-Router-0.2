e3e86b39e3862fdfbc8878c4efeb1821a298ac3b
sadiaprova12 <sadiaprova51717@gmail.com>
2024-04-02 19:47:56 +0600

    friend detail

---
+++ src/components/About/FriendDetail/FriendDetail.jsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+const FriendDetail = () => {
+    return (
+        <div>
+            <h3>Everything about this person is here!</h3>
+            
+        </div>
+    );
+};
+
+export default FriendDetail;
